1eaf053fb8d1271fce79915b6b719a3b315baeb6
Ivan Chekhovskikh <chekhovskikhiv@quadcom.ru>
2021-05-12 00:47:39 +0300

    fixed over move of the long click view

--- app/src/main/java/com/chekh/paysage/core/ui/view/LongClickableCardView.kt
+++ app/src/main/java/com/chekh/paysage/core/ui/view/LongClickableCardView.kt
@@ -1,12 +1,14 @@
 package com.chekh.paysage.core.ui.view
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import androidx.cardview.widget.CardView
 import com.chekh.paysage.core.provider.ui
 import kotlinx.coroutines.*
+import kotlin.math.abs
 
 class LongClickableCardView @JvmOverloads constructor(
     context: Context,
@@ -16,6 +18,7 @@ class LongClickableCardView @JvmOverloads constructor(
 
     private var onLongClickListener: OnLongClickListener? = null
     private var delayedJob: Job? = null
+    private var downPoint = PointF(0f, 0f)
 
     init {
         isLongClickable = true
@@ -35,6 +38,7 @@ class LongClickableCardView @JvmOverloads constructor(
         }
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
+                downPoint = PointF(event.x, event.y)
                 if (delayedJob == null || delayedJob?.isCompleted == true) {
                     delayedJob = GlobalScope.launch(ui) {
                         delay(CLICK_DURATION)
@@ -45,13 +49,22 @@ class LongClickableCardView @JvmOverloads constructor(
                     }
                 }
             }
-            MotionEvent.ACTION_MOVE -> Unit
+            MotionEvent.ACTION_MOVE -> {
+                if (delayedJob?.isActive == true && isOverMove(event.x, event.y)) {
+                    delayedJob?.cancel()
+                }
+            }
             else -> delayedJob?.cancel()
         }
         return false
     }
 
+    private fun isOverMove(moveX: Float, moveY: Float) =
+        abs(downPoint.x - moveX) > AVAILABLE_MOVE_PX ||
+            abs(downPoint.y - moveY) > AVAILABLE_MOVE_PX
+
     private companion object {
         const val CLICK_DURATION = 500L
+        const val AVAILABLE_MOVE_PX = 5
     }
 }
